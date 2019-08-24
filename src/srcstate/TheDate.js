41e7d107ddc2235325d778988940b8b4245678e0
Rayce Tippetts <Raycetip@gmail.com>
2019-08-24 15:36:54 -0700

    setting reading and updating

--- src/srcstate/TheDate.js
+++ src/srcstate/TheDate.js
@@ -1,23 +1,30 @@
 import React from 'react';
 
-class TheDate extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { datetime: new Date() };
-    }
-    componentDidMount() {
-      this.interval = setInterval(() => {
-        console.log('tick')
-      }, 1000)
-    }
-    componentWillUnmount() {
-      clearInterval(this.interval)
-    }
-    render() {
-      return (
-        <div>{this.state.datetime.toLocaleString()}</div>
-      )
-    }
+
+export default class TheDate extends Component {
+constructor(props) {
+    super(props)
+    this.state = { datetime: new Date() };
+    console.log('constructor')
   }
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.interval = setInterval(() => {
+      console.log('setInterval')
+      this.setState({
+        datetime: new Date()
+      })
+    }, 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  render() {
+    console.log('render')
+    return (
+      <div>{this.state.datetime.toLocaleString()}</div>
+    )
+  }
+} 
 
 export default TheDate
